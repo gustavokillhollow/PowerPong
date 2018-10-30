dd53148c696159af18980053e46374f0a80cc4f8
Marco Aurelio Silva Ramos <marco.ramos@agrario.gov.br>
2018-10-30 08:45:52 -0300

    Nome dos jogadores antes de começar o jogo

--- InputNomeJogadores.java
+++ InputNomeJogadores.java
@@ -26,15 +26,8 @@ public class InputNomeJogadores extends World
     public void prepare(){
         IniciarJogo iniciarjogo = new IniciarJogo();
         addObject(iniciarjogo,299,275);
-    }
-    
-    public void act(){
-        ask();
-        
-    }
- 
-    public void ask(){
         String Pong = Greenfoot.ask("Nome do Jogador 1: ");
         String Pong2 = Greenfoot.ask( "Nome Jogador 2: ");
     }
+
 }
